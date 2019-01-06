03385170f2dc8bb9870a8f3cdfcae9d376cb0a54
Александр Пушкин <allpuschkin@yandex.ru>
2019-01-06 17:27:06 +0300

    Самый последний вариант

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ var open = document.querySelector(".open-feedback");
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".feedback-close");
 var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
 var form = popup.querySelector("form");
-var storage = localStorage.getItem("login");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 open.addEventListener("click", function (event) {
     event.preventDefault(event);
@@ -12,16 +21,29 @@ open.addEventListener("click", function (event) {
     if (storage) {
         login.value = storage;
     } else {
-        login.focus();
+        email.focus();
     }
 });
 
 close.addEventListener("click", function (event) {
     event.preventDefault(event);
     popup.classList.remove("feedback-show");
+    popup.classList.remove("feedback-error");
 });
 
-
+form.addEventListener("submit", function(evt) {
+    if (!login.value || !email.value || !text.value) {
+        evt.preventDefault();
+        popup.classList.remove("feedback-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("feedback-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+            localStorage.setItem("email", email.value);
+        }
+    }
+});
 
 window.addEventListener("keydown", function (event) {
     if (event.keyCode == 27) {
@@ -30,4 +52,6 @@ window.addEventListener("keydown", function (event) {
         }
     }
 });
+
+
 
